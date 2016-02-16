29a1217c73f0b6cc19835060c4cea935acfc8297
Ross Bryan <bryanro@gvsu.e.edu>
2016-02-16 18:56:41 -0500

    set up project in intellij. created client. need to do some work on server now

---
+++ client.java
@@ -0,0 +1,88 @@
+/**************************
+ * Ross Bryan and Nate Lentz
+ * Project 2
+ * DNS Resolver
+ ************************/
+import java.io.*;
+import java.net.*;
+
+public class client {
+
+    public static void main(String args[]) throws Exception{
+        DatagramSocket clientSocket = new DatagramSocket();
+        BufferedReader inFromUser =
+                new BufferedReader(new InputStreamReader(System.in));
+        String ip = "";
+        int port = -1;
+        System.out.println("Enter an IP adress: ");
+        ip = inFromUser.readLine();
+        System.out.println("Enter in a socket: ");
+        port = Integer.parseInt(inFromUser.readLine());
+        while (true){
+            System.out.print("Enter a message: ");
+            String message = inFromUser.readLine();
+            System.out.println();
+            byte[] sendData = new byte[1024];
+            sendData=message.getBytes();
+            InetAddress IPAddress = InetAddress.getByName(ip);
+
+            DatagramPacket sendPacket =
+                    new DatagramPacket(sendData,sendData.length,IPAddress,port);
+            clientSocket.send(sendPacket);
+
+
+            byte[] receiveData = new byte[1024];
+            DatagramPacket receivePacket =
+                    new DatagramPacket(receiveData,receiveData.length);
+            clientSocket.receive(receivePacket);
+            String serverMessage = new String(receivePacket.getData());
+            System.out.println("Got from server: "+serverMessage);
+
+            if (message.equals("")){
+                System.out.println("you have requested exit!");
+                System.exit(0);
+            }
+            System.out.println("Message sent to server!");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
